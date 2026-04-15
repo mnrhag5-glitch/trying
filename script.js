9481ac46444e06d436b2437ce1f148e524006461
Gautam Thakur <mnrhag5@gmail.com>
2026-04-15 18:29:06 +0530

    update

--- script.js
+++ script.js
@@ -5,3 +5,20 @@ console.log(name.length);
 
 
 //ye batata hia ki mere naam me kitne words hai -------
+
+
+let sentence = "Hello seju how are you today";
+const newSentencs = sentence.replace("today","baby");
+console.log(newSentencs);
+
+
+//this helps to change any kind of our word-------
+
+
+let invitation = `hy bro tomorow is my bdy
+ ok bro so please come bro hy do 
+ you listen bro hy are you there bro`;
+
+ let Aamantran = invitation.replaceAll('hy','oy dalle')
+ .replaceAll('bro','chutiye');
+ console.log(Aamantran);
