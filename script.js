aedce4aaacce4acc950572bc619f07d272bb311d
Gautam Thakur <mnrhag5@gmail.com>
2026-04-16 11:10:06 +0530

    update

--- script.js
+++ script.js
@@ -1,66 +1,66 @@
 // today i learn about string method-------------
 
-let name="gautam thakur"
-console.log(name.length);
+// let name="gautam thakur"
+// console.log(name.length);
 
 
 //ye batata hia ki mere naam me kitne words hai -------
 
 
-let sentence = "Hello seju how are you today";
-const newSentencs = sentence.replace("today","baby");
-console.log(newSentencs);
+// let sentence = "Hello seju how are you today";
+// const newSentencs = sentence.replace("today","baby");
+// console.log(newSentencs);
 
 
 //this helps to change any kind of our word-------
 
 
-let invitation = `hy bro tomorow is my bdy
- ok bro so please come bro hy do 
- you listen bro hy are you there bro`;
+// let invitation = `hy bro tomorow is my bdy
+//  ok bro so please come bro hy do 
+//  you listen bro hy are you there bro`;
 
- let Aamantran = invitation.replaceAll('hy','oy dalle')
- .replaceAll('bro','chutiye');
- console.log(Aamantran);
+//  let Aamantran = invitation.replaceAll('hy','oy dalle')
+//  .replaceAll('bro','chutiye');
+//  console.log(Aamantran);
 
 
 
  //ok now we are learning (split) ----------
 
 
- let info = "hello my name is gautam thakur"
+//  let info = "hello my name is gautam thakur"
 
- const information =info.split(' ');
+//  const information =info.split(' ');
 
- console.log(information);
+//  console.log(information);
 
 
 
  
  //now we are learning (repaet string method)-----------
 
- let text ="how are you seju ,";
+//  let text ="how are you seju ,";
 
- console.log(text.repeat(2));
+//  console.log(text.repeat(2));
 
 
  //(trim)
 
 
- let trim ="    hy seju what are you doing today   ";
+//  let trim ="    hy seju what are you doing today   ";
 
- const trimation =trim.trim();
+//  const trimation =trim.trim();
 
- console.log(trimation);
+//  console.log(trimation);
  
 
  ///now we are learning concating-----------------
 
- let seju ="hey gautam how are you ? ";
- let gautam ="i am good seju 😁";
+//  let seju ="hey gautam how are you ? ";
+//  let gautam ="i am good seju 😁";
 
- let both =seju.concat(gautam);
- console.log(both);
+//  let both =seju.concat(gautam);
+//  console.log(both);
 
 
  //so today is only this thing i learn and tomorrow  i learn loops ok😁😁
@@ -68,13 +68,13 @@ let invitation = `hy bro tomorow is my bdy
 
  //one time revision--------
 
- let fruit =`mango grapes bnana apple lichi
-  pineapple gauvav orange cocout 
-   papaya watermannle lemon cherry dragonfruit avacado `
+//  let fruit =`mango grapes bnana apple lichi
+//   pineapple gauvav orange cocout 
+//    papaya watermannle lemon cherry dragonfruit avacado `
 
 
-   const AllFruit =fruit.split(" ");
-   console.log(AllFruit);
+   // const AllFruit =fruit.split(" ");
+   // console.log(AllFruit);
 
 
 
@@ -84,26 +84,36 @@ let invitation = `hy bro tomorow is my bdy
 //    let repeatAgain = repeat.repeat(5)
 //    console.log(repeatAgain);
 
-console.log(repeat.repeat(2))
+// console.log(repeat.repeat(2))
 
 
 ///both are right its your choice------
 
-let uppercase ="helo buddy how are you today , tell me";
-const upperCase=uppercase.toUpperCase();
-console.log(upperCase);
+// let uppercase ="helo buddy how are you today , tell me";
+// const upperCase=uppercase.toUpperCase();
+// console.log(upperCase);
 
 
-let lowercase ="JHGH KGKJ ,GLUHK IOIYDYTD YGUHJ FYGFIYF DFTTDVJ ";
-const lowerCase=lowercase.toLowerCase();
-console.log(lowerCase);
+// let lowercase ="JHGH KGKJ ,GLUHK IOIYDYTD YGUHJ FYGFIYF DFTTDVJ ";
+// const lowerCase=lowercase.toLowerCase();
+// console.log(lowerCase);
 
 
 
 //last thig ois concat ------------------
 
-let words ="a b c d e f ";
-let nextWords = "g h i j k l m";
+// let words ="a b c d e f ";
+// let nextWords = "g h i j k l m";
  
-let mixThem=words.concat(nextWords);
-console.log(mixThem);
+// let mixThem=words.concat(nextWords);
+// console.log(mixThem);
+
+
+//today i am learning loops--------------------
+//so print all  even number from 0 to 100-----------
+let number =0;
+for(number=0; number<=100; number++){
+   if(number%2===0){
+      console.log(number ,"is even number");
+   }
+}
