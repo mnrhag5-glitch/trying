b164da358a3692d299af7dc0ca8ba7b76b6a4254
Gautam Thakur <mnrhag5@gmail.com>
2026-04-29 18:46:42 +0530

    add

--- script.js
+++ script.js
@@ -1403,3 +1403,23 @@
 // }console.log(string('yo heloo sdvdvd'))
 
 
+//-----------scope---------------------
+
+
+
+var a=10;
+let b=20;
+const c=30;
+
+if(true){
+   var a=100;
+   let b=200;
+   const c=300;
+
+}
+sam()
+function sam(){var a=1000;
+   let b=2000;
+   const c=3000;
+   
+}
